789bfb35e26a321b4e0ab3b1bf52560172eee4ff
simonLhuillier <sim97@hotmail.fr>
2019-03-18 11:42:07 +0100

    Admin update

--- dashboard/src/components/admin.js
+++ dashboard/src/components/admin.js
@@ -1,60 +1,75 @@
 import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent , Textfield} from 'react-mdl';
+import { Grid, Cell, Tab,Tabs, Textfield, Button} from 'react-mdl';
 
 
 class Admin extends Component {
+  
+  
+  
+  constructor(props) {
+    super(props);
+    this.state={
+    value1:'Name :',
+    value2:'Surname :',
+    value3:'Birth :',
+    value4:'Album : ',
+    value5:'Followers : ',
+    activeTab : 1
+    }
+    }
+  
+  
+  
+  
   render() {
     return(
       <div >
-        <br/>
+        <div className="demo-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab onClick = {()=>{this.setState({value1:'Title : ',value2:'Duration : ',value3:'Listenings : ',value4:'Likes : ',value5:'Track'})}}>Track</Tab>
+                    <Tab onClick = {()=>{this.setState({value1:'Name : ',value2:'Surname : ',value3:'Birth : ',value4:'Album : ',value5:'Followers'})}}>Artist</Tab>
+                    <Tab onClick = {()=>{this.setState({value1:'Title : ',value2:'Release : ',value3:'Type : ',value4:'Cover-URL : ',value5:'Tracks'})}}>Album</Tab>
+                </Tabs>
+            </div>  
         <Grid className="contact-grid" style={{ opacity:'0.8'}}>
           <Cell col={6}>
-            <h2>Admin Space</h2>
-            <br/>
-            <h6>Enter what you want to enter</h6>
+            <h6>{this.state.value1}</h6>
             <Textfield
     onChange={() => {}}
-    label="Text..."
+    label="..."
+    style={{width: '200px'}}
+/>
+<h6>{this.state.value2}</h6>
+<Textfield
+    onChange={() => {}}
+    label="..."
     style={{width: '200px'}}
 />
 
+<h6>{this.state.value3}</h6>
 <Textfield
     onChange={() => {}}
-    pattern="-?[0-9]*(\.[0-9]+)?"
-    error="Input is not a number!"
-    label="Number..."
+    label="..."
     style={{width: '200px'}}
 />
-<h6>Enter what you want to enter</h6>
+<h6>{this.state.value4}</h6>
 <Textfield
     onChange={() => {}}
-    label="Text..."
-    style={{width: '200px'}}
-/>
-
-<Textfield
-    onChange={() => {}}
-    pattern="-?[0-9]*(\.[0-9]+)?"
-    error="Input is not a number!"
-    label="Number..."
-    style={{width: '200px'}}
-/>
-<h6>Enter what you want to enter</h6>
-<Textfield
-    onChange={() => {}}
-    label="Text..."
+    label="..."
     style={{width: '200px'}}
 />
 
+<h6>{this.state.value5}</h6>
 <Textfield
     onChange={() => {}}
-    pattern="-?[0-9]*(\.[0-9]+)?"
-    error="Input is not a number!"
-    label="Number..."
+    label="..."
     style={{width: '200px'}}
 />
-          </Cell>
+<br/>
+          <Button ripple>Add</Button>
          
+          </Cell>
+          
         </Grid>
       </div>
     )
@@ -62,3 +77,50 @@ class Admin extends Component {
 }
 
 export default Admin;
+
+
+
+/*
+
+
+render() {
+return (
+<div className="profile "> 
+<h2>Ajout {this.state.value5}</h2> 
+<form className="text-center">
+<label>{this.state.value1} 
+<input type="text" name="{this.state.value1} " />
+</label>
+
+
+<input type="submit" value="Submit" />
+<br/>
+<button
+type="button"
+id="bouton"
+onClick = {()=>{this.setState({value1:'Title : ',value2:'Duration : ',value3:'Listenings : ',value4:'Likes : ',value5:'Track'})}}
+>
+Add Track
+</button>
+<button
+type="button"
+id="bouton"
+onClick = {()=>{this.setState({value1:' Title : ',value2:'Release : ',value3:'Genre : ',value4:'Cover Url : ',value5:'Album'})}}
+>
+Add Album
+</button>
+<button
+type="button"
+id="bouton"
+onClick = {()=>{this.setState({value1:' Surname : ',value2:'Name : ',value3:'Followers : ',value4:'Undefined : ',value5:'Artist'})}}
+>
+Add Artist
+</button>
+</form>
+
+</div>
+);
+}
+}
+
+export default admin;*/
---
+++ dashboard/src/models/dashboard.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+/*
+var ArtistSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    city: {
+        type: String
+    }
+});
+*/
